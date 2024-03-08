8e20dc13727fb7edc39a67cf21e8abf79a6efaca
Jaden Torres <156242643+Jaden-torres@users.noreply.github.com>
2024-03-07 19:45:52 -0500

    Midterm upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        Inbox = New TextBox()
+        Answer = New GroupBox()
+        Option1 = New RadioButton()
+        Option2 = New RadioButton()
+        Label3 = New Label()
+        Convert = New Button()
+        Clear = New Button()
+        ExitProgram = New Button()
+        Ignore = New RadioButton()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(483, 55)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(359, 65)
+        Label1.TabIndex = 0
+        Label1.Text = "Converter App"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Semibold", 20F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(367, 260)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(679, 54)
+        Label2.TabIndex = 1
+        Label2.Text = "Enter a value and choose conversion"
+        ' 
+        ' Inbox
+        ' 
+        Inbox.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Inbox.Location = New Point(1097, 274)
+        Inbox.Name = "Inbox"
+        Inbox.Size = New Size(265, 39)
+        Inbox.TabIndex = 2
+        ' 
+        ' Answer
+        ' 
+        Answer.Font = New Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Answer.Location = New Point(764, 732)
+        Answer.Name = "Answer"
+        Answer.Size = New Size(784, 81)
+        Answer.TabIndex = 3
+        Answer.TabStop = False
+        Answer.Text = "GroupBox1"
+        ' 
+        ' Option1
+        ' 
+        Option1.AutoSize = True
+        Option1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Option1.Location = New Point(624, 465)
+        Option1.Name = "Option1"
+        Option1.Size = New Size(324, 52)
+        Option1.TabIndex = 4
+        Option1.TabStop = True
+        Option1.Text = "Inches to Meters"
+        Option1.UseVisualStyleBackColor = True
+        ' 
+        ' Option2
+        ' 
+        Option2.AutoSize = True
+        Option2.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Option2.Location = New Point(624, 521)
+        Option2.Name = "Option2"
+        Option2.Size = New Size(324, 52)
+        Option2.TabIndex = 5
+        Option2.TabStop = True
+        Option2.Text = "Meters to Inches"
+        Option2.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(603, 414)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(395, 48)
+        Label3.TabIndex = 6
+        Label3.Text = "Convert Measurement"
+        ' 
+        ' Convert
+        ' 
+        Convert.Location = New Point(30, 929)
+        Convert.Name = "Convert"
+        Convert.Size = New Size(241, 94)
+        Convert.TabIndex = 7
+        Convert.Text = "Convert"
+        Convert.UseVisualStyleBackColor = True
+        ' 
+        ' Clear
+        ' 
+        Clear.Location = New Point(483, 929)
+        Clear.Name = "Clear"
+        Clear.Size = New Size(241, 94)
+        Clear.TabIndex = 8
+        Clear.Text = "Clear"
+        Clear.UseVisualStyleBackColor = True
+        ' 
+        ' ExitProgram
+        ' 
+        ExitProgram.Location = New Point(947, 929)
+        ExitProgram.Name = "ExitProgram"
+        ExitProgram.Size = New Size(241, 94)
+        ExitProgram.TabIndex = 9
+        ExitProgram.Text = "Exit"
+        ExitProgram.UseVisualStyleBackColor = True
+        ' 
+        ' Ignore
+        ' 
+        Ignore.AutoSize = True
+        Ignore.Location = New Point(483, 732)
+        Ignore.Name = "Ignore"
+        Ignore.Size = New Size(89, 29)
+        Ignore.TabIndex = 10
+        Ignore.TabStop = True
+        Ignore.Text = "Ignore"
+        Ignore.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(12, 129)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(274, 235)
+        PictureBox1.TabIndex = 11
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1580, 1143)
+        Controls.Add(PictureBox1)
+        Controls.Add(Ignore)
+        Controls.Add(ExitProgram)
+        Controls.Add(Clear)
+        Controls.Add(Convert)
+        Controls.Add(Label3)
+        Controls.Add(Option2)
+        Controls.Add(Option1)
+        Controls.Add(Answer)
+        Controls.Add(Inbox)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Inbox As TextBox
+    Friend WithEvents Answer As GroupBox
+    Friend WithEvents Option1 As RadioButton
+    Friend WithEvents Option2 As RadioButton
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Convert As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents ExitProgram As Button
+    Friend WithEvents Ignore As RadioButton
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,110 @@
+﻿'Midterm Project'
+'Jaden Torres'
+'GUI Development Class'
+'Sources: none'
+
+Public Class Form1
+
+    Dim strInput As String 'input variable gathered'
+    Dim decInput As Decimal 'input variable parsed into a decimal'
+    Dim OneTool As Decimal = 39.3700787 'variable used to calculate inches to meters'
+    Dim TwoTool As Decimal = 39.3700787 'variable used to calculate meters to inches'
+    Dim DecOneTool As Decimal ' variable to capture result of OneTool and temp'
+    Dim DecTwoTool As Decimal ' variable to capture result of TwoTool and temp'
+    Dim AnswerTool As Decimal 'variable to house the definite answer'
+    Dim temp As Integer 'Holds input variable for compute use'
+    Dim counter As Decimal = 1 'Use to handle the odd radio button error once the form loads.'
+    Dim proc As Decimal 'Used to determine what radio button is pressed'
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'loads form'
+        Ignore.PerformClick() 'performs ignore button click'
+        Ignore.Hide() 'Hides ignore button'
+    End Sub
+
+    Private Sub Inbox_TextChanged(sender As Object, e As EventArgs) Handles Inbox.TextChanged
+
+    End Sub
+
+    Private Sub Answer_Enter(sender As Object, e As EventArgs) Handles Answer.Enter
+
+    End Sub
+
+    Private Sub Convert_Click(sender As Object, e As EventArgs) Handles Convert.Click 'This is what happens when the convert button is clicked'
+        strInput = Inbox.Text 'Captures input of the Inbox'
+        If Decimal.TryParse(strInput, decInput) = False And counter = 0 Then 'Parses input to decimal form, checks to see if string can be parsed'
+            MsgBox("Error: Value you have entered is not numeric. Please try again")
+            Inbox.ResetText() 'resets text box'
+            Answer.ResetText() 'resets group box'
+            Exit Sub
+        Else
+            If IsNumeric(strInput) = False Then 'Checks to see if input is numeric, if not then run error msg'
+                MsgBox("Error: Value you have entered is not numeric. Please try again")
+                Inbox.ResetText()
+                Answer.ResetText()
+                Exit Sub
+            Else
+                If decInput < 0 Then 'Checks to see if the input is negative, if negative then run error msg'
+                    MsgBox("Error: Value you have entered is negative. Please try again")
+                    Inbox.ResetText()
+                    Answer.ResetText()
+                    Exit Sub
+                End If
+            End If
+        End If
+        temp = 0 'resets temp from previous value'
+        temp = decInput ' adds new value to temp'
+        Compute() 'calling compute class'
+    End Sub
+
+    Private Sub Compute() 'This class runs the calculations'
+        If proc = 1 Then ' if the first radio button is pressed' 
+            AnswerTool = 0
+            DecOneTool = Math.Round(temp / OneTool, 3)
+            AnswerTool = DecOneTool
+        Else
+            If proc = 2 Then ' if the second radio button is pressed'
+                AnswerTool = 0
+                DecTwoTool = Math.Round(TwoTool * temp, 3)
+                AnswerTool = DecTwoTool
+            End If
+        End If
+        Stat_Output() ' calls stat_output class'
+    End Sub
+
+    Private Sub Stat_Output() ' formats and sends calculations to answer groupbox'
+        If proc = 1 Then 'format for the first radio button option'
+            Answer.Text = decInput.ToString() + " inches is " + AnswerTool.ToString() + " meters."
+        Else
+            If proc = 2 Then 'format for the second radio button option'
+                Answer.Text = decInput.ToString() + " meters is " + AnswerTool.ToString() + " inches."
+            End If
+        End If
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click 'This is what happens when the clear button is pressed'
+        Inbox.ResetText()
+        Answer.ResetText()
+        proc = 0
+    End Sub
+
+    Private Sub ExitProgram_Click(sender As Object, e As EventArgs) Handles ExitProgram.Click ' This is what happens when the Exit button is pressed'
+        Close() 'exit program'
+    End Sub
+
+    Private Sub Ignore_CheckedChanged(sender As Object, e As EventArgs) Handles Ignore.CheckedChanged 'This button is to be ignored, this helps that weird button issue off startup'
+        counter = counter - 1
+    End Sub
+
+    Private Sub Option1_Click(sender As Object, e As EventArgs) Handles Option1.Click ' this is what happens when radio button one is clicked'
+        proc = 1 'sets proc a value to be called on later'
+    End Sub
+
+    Private Sub Option2_Click(sender As Object, e As EventArgs) Handles Option2.Click ' this is what happens when radio button two is clicked'
+        proc = 2 'sets proc a value to be called on later'
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
